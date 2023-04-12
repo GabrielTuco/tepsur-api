b69efde25d14feda6556e971f8153ccf3190cdc6
Arnold Olanda <81376643+ArnoldOlanda@users.noreply.github.com>
2023-04-12 19:23:29 +0000

    Add hasPermissionRole middleware

--- src/Auth/controllers/auth.controller.ts
+++ src/Auth/controllers/auth.controller.ts
@@ -3,4 +3,22 @@ import { AuthService } from "../services/auth.service";
 
 const authService = new AuthService();
 
-export class AuthController {}
+export class AuthController {
+    async postLogin(req:Request, res:Response){
+        //Obtener el id del usuario
+        //determinar si es alumno, docente, secretaria o administrador
+        //validar si el usuario tiene un registro de alumno, docente, secretaria o administador vinculado
+        //Validar la sede a la que pertenece el usuario
+        //Validar la contraseña
+        //Generar token
+        //Enviar respuesta con el token de autenticacion y la informacion del usuario y 
+        try {
+            const {usuario, password, role} = req.body
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                msg:"contact the administrator"
+            })
+        }
+    }
+}
---
+++ src/middlewares/hasPermissionRole.ts
@@ -0,0 +1,10 @@
+import { Rol } from "../Auth/entity/Rol.entity"
+
+export const hasPermissionRole = async(codRole:number, validRole:string)=>{
+    const role = await Rol.findOne({where:{nombre:validRole}})
+
+    if(role!.id !== codRole){
+        throw new Error("No tienes privilegios para realizar esta operacion")
+    }
+    return true
+}
