80ccb49549d1902f19c99a1deff1da6d71696463
ArnoldOlanda <olanda188@gmail.com>
2023-07-10 07:19:31 -0700

    test commit

--- src/Auth/entity/Permiso.entity.ts
+++ src/Auth/entity/Permiso.entity.ts
@@ -1,6 +1,5 @@
-import { Column, Entity, OneToMany } from "typeorm";
+import { Column, Entity } from "typeorm";
 import { EntityBase } from "../../entity/EntityBase";
-import { Usuario } from "./Usuario.entity";
 
 @Entity()
 export class Permiso extends EntityBase {
