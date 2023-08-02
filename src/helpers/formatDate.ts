b22a32053abaef3825bb8d2563a8056b1b4d9a12
ArnoldOlanda <olanda188@gmail.com>
2023-08-01 22:38:54 -0500

    Format on fecha inscripcion in PDFS

---
+++ src/helpers/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatedTime = (date: Date): string => {
+    const newDate = new Date(date);
+    const days = newDate.toLocaleDateString();
+    const month = newDate.getMonth() + 1;
+    const year = newDate.getFullYear();
+
+    return `${days}/${month}/${year}`;
+};
+
+console.log(formatedTime(new Date()));
