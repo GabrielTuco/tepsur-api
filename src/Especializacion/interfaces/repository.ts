811ccee3f4160e34cbedabe46e9890092fb6dd38
ArnoldOlanda <olanda188@gmail.com>
2023-08-30 19:26:36 -0500

    Fix especializacion repository

--- src/Especializacion/interfaces/repository.ts
+++ src/Especializacion/interfaces/repository.ts
@@ -1,14 +1,15 @@
+import { RegisterEspecializacionDto } from "../dto/registerEspecializacion.dto";
 import { Especializacion } from "../entity/Especializacion.entity";
 import { MatriculaEspecializacion } from "../entity/MatriculaEspecializacion.entity";
-import { EspecializacionDTO, MatEspeDTO } from "./dtos";
+import { MatEspeDTO } from "./dtos";
 
 export interface EspecializacionRepository {
-    register(data: EspecializacionDTO): Promise<Especializacion>;
+    register(data: RegisterEspecializacionDto): Promise<Especializacion>;
     listAll(): Promise<Especializacion[]>;
     findByUuid(uuid: string): Promise<Especializacion>;
     update(
         uuid: string,
-        data: Partial<EspecializacionDTO>
+        data: Partial<RegisterEspecializacionDto>
     ): Promise<Especializacion>;
     delete(uuid: string): Promise<Especializacion>;
 }
