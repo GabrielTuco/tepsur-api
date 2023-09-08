1f9b58a1a7ae3472ba933aba7b25505b121b3d47
ArnoldOlanda <olanda188@gmail.com>
2023-09-08 00:18:11 -0500

    Add documentation for certificados endpoints

--- src/Certificados/routes/certificado.routes.ts
+++ src/Certificados/routes/certificado.routes.ts
@@ -12,6 +12,64 @@ const router = Router();
 const certificadoService = new CertificadoService();
 const certificadoController = new CertificadoController(certificadoService);
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Certificado:
+ *         properties:
+ *            uuid:
+ *                type: string
+ *                format: uuid
+ *            description:
+ *                type: string
+ *            url:
+ *                type: string
+ *            matricula:
+ *                type: object
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Certificados
+ *  description: Endpoints para los certificados
+ */
+
+/**
+ * @swagger
+ * /certificados:
+ *  post:
+ *      summary: Registar un nuevo certificado
+ *      tags: [Certificados]
+ *      parameters:
+ *          - $ref: '#/components/parameters/token'
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              multipart/form-data:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          image:
+ *                              type: string
+ *                              format: binary
+ *                          descripcion:
+ *                              type: string
+ *                          matriculaUuid:
+ *                              type: string
+ *                              format: uuid
+ *      responses:
+ *          200:
+ *              description: El certificado registrado
+ *              content:
+ *                  application/json:
+ *                       schema:
+ *                          type: object
+ *          500:
+ *              description: Error de servidor
+ *
+ */
 router.post(
     "/",
     [
@@ -25,22 +83,134 @@ router.post(
     certificadoController.postCertificado
 );
 
+/**
+ * @swagger
+ * /certificados/{uuid}:
+ *  get:
+ *      summary: Buscar un certificado por uuid
+ *      tags: [Certificados]
+ *      parameters:
+ *          - $ref: '#/components/parameters/token'
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            schema:
+ *              type: string
+ *              format: uuid
+ *            description: uuid de la matricula
+ *      responses:
+ *          200:
+ *              description: El certificado buscado
+ *              content:
+ *                  application/json:
+ *                       schema:
+ *                          $ref: '#/components/schemas/Certificado'
+ *
+ *          500:
+ *              description: Error de servidor
+ *
+ */
 router.get(
     "/:uuid",
     [validateJWT, checkAuthRole([ROLES.ROOT, ROLES.ADMIN, ROLES.SECRE])],
     certificadoController.getByUuid
 );
+
+/**
+ * @swagger
+ * /certificados/matricula/{uuid}:
+ *  get:
+ *      summary: Buscar los certificados de una matricula
+ *      tags: [Certificados]
+ *      parameters:
+ *          - $ref: '#/components/parameters/token'
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            schema:
+ *              type: string
+ *              format: uuid
+ *            description: uuid de la matricula
+ *      responses:
+ *          200:
+ *              description: El listado de certificados
+ *              content:
+ *                  application/json:
+ *                       schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Certificado'
+ *
+ *          500:
+ *              description: Error de servidor
+ *
+ */
 router.get(
     "/matricula/:uuid",
     [validateJWT, checkAuthRole([ROLES.ROOT, ROLES.ADMIN, ROLES.SECRE])],
     certificadoController.getByMatricula
 );
+
+/**
+ * @swagger
+ * /certificados:
+ *  get:
+ *      summary: Listar todos los certificados
+ *      tags: [Certificados]
+ *      parameters:
+ *          - $ref: '#/components/parameters/token'
+ *      responses:
+ *          200:
+ *              description: El listado de certificados
+ *              content:
+ *                  application/json:
+ *                       schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Certificado'
+ *
+ *          500:
+ *              description: Error de servidor
+ *
+ */
 router.get(
     "/",
     [validateJWT, checkAuthRole([ROLES.ROOT, ROLES.ADMIN, ROLES.SECRE])],
     certificadoController.getAll
 );
 
+/**
+ * @swagger
+ * /certificados/{uuid}:
+ *  delete:
+ *      summary: Buscar los certificados de una matricula
+ *      tags: [Certificados]
+ *      parameters:
+ *          - $ref: '#/components/parameters/token'
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            schema:
+ *              type: string
+ *              format: uuid
+ *            description: uuid de la matricula
+ *      responses:
+ *          200:
+ *              description: El listado de certificados
+ *              content:
+ *                  application/json:
+ *                       schema:
+ *                          type: object
+ *                          properties:
+ *                              uuid:
+ *                                  type: string
+ *                                  format: uuid
+ *                                  description: el uuid del certificado eliminado
+ *
+ *          500:
+ *              description: Error de servidor
+ *
+ */
 router.delete(
     "/:uuid",
     [
